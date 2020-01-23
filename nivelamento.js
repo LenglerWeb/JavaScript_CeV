d82e50c5394aabf78d1f10f5734f4f81dc80f3b3
lenglerweb <lenglerweb@gmail.com>
2020-01-23 14:47:33 -0300

    Iniciando o repositório

---
+++ nivelamento.js
@@ -0,0 +1,7 @@
+function filosofoHipster (profissao, nacionalidade, quilometros){
+var atividade = 'Músico';
+var ondeNasceu = 'Brasil';
+var quantoAndou = 2;
+return profissao == atividade && nacionalidade == ondeNasceu && quilometros >= quantoAndou; 
+}
+ 
