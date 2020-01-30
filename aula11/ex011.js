b9089986b049cbf7ae505b37d92b8a00ddfba054
lenglerweb <lenglerweb@gmail.com>
2020-01-30 15:31:53 -0300

    Adicionando novas aulas

---
+++ aula11/ex011.js
@@ -0,0 +1,12 @@
+//console.log('O console.log funcionou!');
+var vel = 60
+
+
+console.log(`A sua velocidade é ${vel}Km/h`);
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida de 60Km/h. MULTADO!')
+
+    } else {
+        console.log('Dirija sempre com cinto de segurança')
+    }
+
