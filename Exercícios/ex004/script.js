c099d227b051882f9ada8b38cdfd28485a58e534
lenglerweb <lenglerweb@gmail.com>
2020-02-05 15:05:36 -0300

    Add novas linhas de código

---
+++ Exercícios/ex004/script.js
@@ -0,0 +1,16 @@
+function verificar() {
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passos = document.getElementById('passos')
+    var res = document.querySelector('div#res')
+
+    var c = 1
+    while (c <= fim){
+        console.log(c)
+        c += 1
+    }
+
+    //res.innerHTML()
+
+}
+
