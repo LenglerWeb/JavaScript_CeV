6a6a53b57db4a326e4628b3c21c2172000775399
lenglerweb <lenglerweb@gmail.com>
2020-02-05 11:58:16 -0300

    inserindo

--- nivelamento.js
+++
@@ -1,7 +0,0 @@
-function filosofoHipster (profissao, nacionalidade, quilometros){
-var atividade = 'Músico';
-var ondeNasceu = 'Brasil';
-var quantoAndou = 2;
-return profissao == atividade && nacionalidade == ondeNasceu && quilometros >= quantoAndou; 
-}
- 
